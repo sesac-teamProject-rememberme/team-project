daa51fd97a124816a15a554fa04dada2471bd505
Ohjju <ojh011223@naver.com>
2023-08-12 13:41:29 +0900

    section3 animation

--- js/about1.js
+++ js/about1.js
@@ -10,30 +10,16 @@ let temp1 = document.querySelector(".temp1");
 let temp2 = document.querySelector(".temp2");
 let temp3 = document.querySelector(".temp3");
 
-// section3 스크롤하면 애니메이션 실행
-const saDefaultMargin = 300;
-let saTriggerMargin = 0;
+// section3 스크롤하면 글씨 나타나는 애니메이션
+let margin = 700;
+//window.innerheight = 739px
 let saTriggerHeight = 0;
-const saElementList = document.querySelectorAll(".sa");
-
 const saFunc = function () {
   for (let index = 0; index < 5; index++) {
     if (!a[index].classList.contains("show")) {
-      if (a[index].dataset.saMargin) {
-        saTriggerMargin = parseInt(a[index].dataset.saMargin);
-      } else {
-        saTriggerMargin = saDefaultMargin;
-      }
-      // 기준 => article3-1의 sa-trigger
-      if (a[index].dataset.saTrigger) {
-        saTriggerHeight =
-          document
-            .querySelector(a[index].dataset.saTrigger)
-            .getBoundingClientRect().top + saTriggerMargin;
-      } else {
-        // saTriggerHeight =a[index].getBoundingClientRect().top + saTriggerMargin;
-        saTriggerHeight = a[index].getBoundingClientRect().top;
-      }
+      saTriggerHeight = a[index].getBoundingClientRect().top + margin;
+      console.log(index);
+      console.log(saTriggerHeight);
 
       if (window.innerHeight > saTriggerHeight) {
         a[index].classList.add("show");
@@ -42,5 +28,4 @@ const saFunc = function () {
   }
 };
 
-window.addEventListener("load", saFunc);
 window.addEventListener("scroll", saFunc);
