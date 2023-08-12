d2376590d330c23c0eb7148e730f27f934167bb5
Ohjju <ojh011223@naver.com>
2023-08-13 00:07:58 +0900

    animation2

--- js/about1.js
+++ js/about1.js
@@ -12,7 +12,7 @@ let temp2 = document.querySelector(".temp2");
 let temp3 = document.querySelector(".temp3");
 
 // section3 스크롤하면 글씨 나타나는 애니메이션
-let margin = 730;
+let margin = 700;
 let saTriggerHeight = 0;
 const saFunc = function () {
   for (let index = 0; index < 6; index++) {
@@ -22,10 +22,16 @@ const saFunc = function () {
       if (window.innerHeight > saTriggerHeight) {
         a[index].classList.add("show");
       }
+      // if (index >= 1) {
+      //   if (a[index - 1].classList.contains("show")) {
+      //     a[index - 1].classList.remove("show");
+      //   }
+      // }
     }
   }
 };
 
+// window.addEventListener("onload", saFunc);
 window.addEventListener("scroll", saFunc);
 
 // 스크롤 할 때마다 함수가 실행됨 (여러번 반복됨)
@@ -48,53 +54,13 @@ window.addEventListener("scroll", saFunc);
 // const boxElemList = document.querySelectorAll(".sa");
 // boxElemList.forEach((a) => observer1.observe(a));
 
-// section7 스크롤시 box 올라오는 애니메이션
-let box1 = document.querySelector(".box1");
-let box2 = document.querySelector(".box2");
-let box3 = document.querySelector(".box3");
-
-let b1_margin = 250;
-let b1_height = 0;
-const Func1 = function () {
-  b1_height = box1.getBoundingClientRect().top + b1_margin;
-  if (window.innerHeight > b1_height) {
-    box1.style.opacity = "1";
-    box1.style.transition = "1.2s";
-    box1.style.margin = "0";
-  }
-};
-window.addEventListener("scroll", Func1);
-
-let b2_margin = 400;
-let b2_height = 0;
-const Func2 = function () {
-  b2_height = box2.getBoundingClientRect().top + b2_margin;
-  if (window.innerHeight > b2_height) {
-    box2.style.opacity = "1";
-    box2.style.transition = "1.2s";
-    box2.style.margin = "0";
-  }
-};
-
-window.addEventListener("scroll", Func2);
-let b3_margin = 180;
-let b3_height = 0;
-const Func3 = function () {
-  b3_height = box3.getBoundingClientRect().top + b3_margin;
-  if (window.innerHeight > b3_height) {
-    box3.style.opacity = "1";
-    box3.style.transition = "1.2s";
-    box3.style.margin = "0";
-  }
-};
-window.addEventListener("scroll", Func3);
-
 // section4 스크롤시 애니메이션
 let section4 = document.querySelector(".section4");
 let a4_1 = document.querySelector(".article4-1");
 let a4_2 = document.querySelector(".article4-2");
 let a4_3 = document.querySelector(".article4-3");
 let a4_4 = document.querySelector(".article4-4");
+let cloud = document.querySelector("#cloud");
 
 let a4_1margin = 150;
 let a4_2margin = 100;
@@ -111,6 +77,9 @@ const Func4_1 = function () {
     a4_1.style.opacity = "1";
     a4_1.style.transition = "1s";
     a4_1.style.marginTop = "40px";
+    cloud.style.transform = "translate(500px, 0px)";
+    cloud.style.scale = "1.6";
+    cloud.style.transition = "1.5s";
   }
 };
 window.addEventListener("scroll", Func4_1);
@@ -119,7 +88,7 @@ const Func4_2 = function () {
   a4_2height = a4_2.getBoundingClientRect().top + a4_2margin;
   if (window.innerHeight > a4_2height) {
     a4_2.style.opacity = "1";
-    a4_2.style.transition = "1.5s";
+    a4_2.style.transition = "1.2s";
     a4_2.style.margin = "0";
   }
 };
@@ -129,7 +98,7 @@ const Func4_3 = function () {
   a4_3height = a4_3.getBoundingClientRect().top + a4_3margin;
   if (window.innerHeight > a4_3height) {
     a4_3.style.opacity = "1";
-    a4_3.style.transition = "1.5s";
+    a4_3.style.transition = "1.2s";
   }
 };
 window.addEventListener("scroll", Func4_3);
@@ -148,10 +117,11 @@ let section5 = document.querySelector(".section5");
 let a5_1 = document.querySelector(".article5-1");
 let a5_2 = document.querySelector(".article5-2");
 let a5_3 = document.querySelector(".article5-3");
+let trash = document.querySelector("#trash");
 
 let a5_1margin = 200;
 let a5_2margin = 100;
-let a5_3margin = 200;
+let a5_3margin = 150;
 let a5_1height = 0;
 let a5_2height = 0;
 let a5_3height = 0;
@@ -160,7 +130,7 @@ const Func5_1 = function () {
   a5_1height = a5_1.getBoundingClientRect().top + a5_1margin;
   if (window.innerHeight > a5_1height) {
     a5_1.style.opacity = "1";
-    a5_1.style.transition = "1.5s";
+    a5_1.style.transition = "1.2s";
     a5_1.style.margin = "0";
   }
 };
@@ -170,7 +140,7 @@ const Func5_2 = function () {
   a5_2height = a5_2.getBoundingClientRect().top + a5_2margin;
   if (window.innerHeight > a5_2height) {
     a5_2.style.opacity = "1";
-    a5_2.style.transition = "1.5s";
+    a5_2.style.transition = "1.2s";
     a5_2.style.margin = "0";
   }
 };
@@ -198,7 +168,7 @@ const Func6_1 = function () {
   a6_1height = a6_1.getBoundingClientRect().top + a6_1margin;
   if (window.innerHeight > a6_1height) {
     a6_1.style.opacity = "1";
-    a6_1.style.transition = "1.5s";
+    a6_1.style.transition = "1.2s";
     a6_1.style.margin = "0";
   }
 };
@@ -208,8 +178,49 @@ const Func6_2 = function () {
   a6_2height = a6_2.getBoundingClientRect().top + a6_2margin;
   if (window.innerHeight > a6_2height) {
     a6_2.style.opacity = "1";
-    a6_2.style.transition = "1.5s";
+    a6_2.style.transition = "1.2s";
     a6_2.style.margin = "0";
   }
 };
 window.addEventListener("scroll", Func6_2);
+
+// section7 스크롤시 box 올라오는 애니메이션
+let box1 = document.querySelector(".box1");
+let box2 = document.querySelector(".box2");
+let box3 = document.querySelector(".box3");
+
+let b1_margin = 200;
+let b1_height = 0;
+const Func1 = function () {
+  b1_height = box1.getBoundingClientRect().top + b1_margin;
+  if (window.innerHeight > b1_height) {
+    box1.style.opacity = "1";
+    box1.style.transition = "1.2s";
+    box1.style.margin = "0";
+  }
+};
+window.addEventListener("scroll", Func1);
+
+let b2_margin = 300;
+let b2_height = 0;
+const Func2 = function () {
+  b2_height = box2.getBoundingClientRect().top + b2_margin;
+  if (window.innerHeight > b2_height) {
+    box2.style.opacity = "1";
+    box2.style.transition = "1.2s";
+    box2.style.margin = "0";
+  }
+};
+
+window.addEventListener("scroll", Func2);
+let b3_margin = 50;
+let b3_height = 0;
+const Func3 = function () {
+  b3_height = box3.getBoundingClientRect().top + b3_margin;
+  if (window.innerHeight > b3_height) {
+    box3.style.opacity = "1";
+    box3.style.transition = "1.2s";
+    box3.style.margin = "0";
+  }
+};
+window.addEventListener("scroll", Func3);
