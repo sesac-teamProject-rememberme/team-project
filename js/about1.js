7dfe17468eb2cf3e74132cc404ed83f43446beae
Ohjju <ojh011223@naver.com>
2023-08-14 15:49:46 +0900

    페이지수정

--- js/about1.js
+++ js/about1.js
@@ -1,22 +1,105 @@
-// 스크롤 할 때마다 함수가 실행됨 (여러번 반복됨)
+// section3 스크롤하면 글씨 나타나는 애니메이션
+let s3 = document.querySelector(".section3");
+let a3_0 = document.querySelector(".article3-0");
+let a3_1 = document.querySelector(".article3-1");
+let a3_2 = document.querySelector(".article3-2");
+let a3_3 = document.querySelector(".article3-3");
+let a3_4 = document.querySelector(".article3-4");
+let a3_5 = document.querySelector(".article3-5");
+let a3_6 = document.querySelector(".article3-6");
+const a = [a3_0, a3_1, a3_2, a3_3, a3_4, a3_5];
 
-// let observer1 = new IntersectionObserver((e) => {
-//   e.forEach((a) => {
-//     // 관찰 대상이 뷰포트에 들어온 경우
-//     if (a.intersectionRatio > 0) {
-//       // 클래스에 show 추가
-//       a.target.classList.add("show");
-//     }
-//     // 관찰 대상이 뷰포트 밖으로 나가는 경우
-//     else {
-//       // 클래스에서 show 제거
-//       a.target.classList.remove("show");
-//     }
-//   });
-// });
+let t1 = document.querySelector(".article3-1 .temp1");
+let t2 = document.querySelector(".article3-2 .temp2");
+let t3 = document.querySelector(".article3-3 .temp3");
 
-// const boxElemList = document.querySelectorAll(".sa");
-// boxElemList.forEach((a) => observer1.observe(a));
+let a0 = a3_0.offsetHeight;
+let a1 = a3_1.offsetHeight;
+let a2 = a3_2.offsetHeight;
+let a3 = a3_3.offsetHeight;
+let a4 = a3_4.offsetHeight;
+let a5 = a3_5.offsetHeight;
+let a6 = a3_6.offsetHeight;
+
+let a0_1 = a0 + a1;
+let a0_2 = a0 + a1 + a2;
+let a0_3 = a0 + a1 + a2 + a3;
+let a0_4 = a0 + a1 + a2 + a3 + a4;
+let a0_5 = a0 + a1 + a2 + a3 + a4 + a5;
+let a0_6 = a0 + a1 + a2 + a3 + a4 + a5 + a6;
+
+window.addEventListener("wheel", (e) => {
+  let wheel = e.wheelDeltaY;
+  if (wheel < 0) {
+    if (scrollY > height1 + 200 && scrollY <= a0_1) {
+      a3_0.classList.add("show");
+    } else if (scrollY > a0_1 + 200 && scrollY <= a0_2) {
+      a3_1.classList.add("show");
+      a3_0.classList.remove("show");
+      t1.classList.add("font_17");
+      t1.style.transition = "0.5s";
+    } else if (scrollY > a0_2 + 200 && scrollY <= a0_3) {
+      a3_2.classList.add("show");
+      a3_1.classList.remove("show");
+      t2.classList.add("font_17");
+      t2.style.transition = "0.5s";
+    } else if (scrollY > a0_3 + 200 && scrollY <= a0_4) {
+      a3_3.classList.add("show");
+      a3_2.classList.remove("show");
+      t3.classList.add("font_17");
+      t3.style.transition = "0.5s";
+    } else if (scrollY > a0_4 + 200 && scrollY <= a0_5) {
+      a3_4.classList.add("show");
+      a3_3.classList.remove("show");
+    } else if (scrollY > a0_5 + 200 && scrollY <= a0_6) {
+      a3_5.classList.add("show");
+      a3_4.classList.remove("show");
+      s3.style.opacity = "1";
+      s3.style.transition = "0.5s";
+    } else if (scrollY > a0_6 + 200) {
+      a3_5.classList.remove("show");
+      s3.style.opacity = "0";
+      s3.style.transition = "0.5s";
+    }
+  } else if (wheel > 0) {
+    if (scrollY > height1 + 200 && scrollY <= a0_1) {
+      a3_0.classList.remove("show");
+    } else if (scrollY > a0_1 + 200 && scrollY <= a0_2) {
+      a3_0.classList.add("show");
+      a3_1.classList.remove("show");
+      t1.classList.remove("font_17");
+      t1.style.transition = "0.5s";
+    } else if (scrollY > a0_2 + 200 && scrollY <= a0_3) {
+      a3_1.classList.add("show");
+      a3_2.classList.remove("show");
+      t1.classList.add("font_17");
+      t1.style.transition = "0.5s";
+      t2.classList.remove("font_17");
+      t2.style.transition = "0.5s";
+    } else if (scrollY > a0_3 + 200 && scrollY <= a0_4) {
+      a3_2.classList.add("show");
+      a3_3.classList.remove("show");
+      t2.classList.add("font_17");
+      t2.style.transition = "0.5s";
+      t3.classList.remove("font_17");
+      t3.style.transition = "0.5s";
+    } else if (scrollY > a0_4 + 200 && scrollY <= a0_5) {
+      a3_3.classList.add("show");
+      a3_4.classList.remove("show");
+      t3.classList.add("font_17");
+      t3.style.transition = "0.5s";
+    } else if (scrollY > a0_5 + 200 && scrollY <= a0_6) {
+      a3_4.classList.add("show");
+      a3_5.classList.remove("show");
+      s3.style.opacity = "1";
+      s3.style.transition = "0.5s";
+    } else if (scrollY > a0_6 + 200) {
+      a3_5.classList.add("show");
+      s3.style.opacity = "0";
+      s3.style.transition = "0.5s";
+    }
+  }
+});
 
 // section4 스크롤시 애니메이션
 let a4_1 = document.querySelector(".article4-1");
@@ -39,7 +122,7 @@ const Func4_1 = function () {
   if (window.innerHeight > a4_1height) {
     a4_1.style.opacity = "1";
     a4_1.style.transition = "1s";
-    a4_1.style.marginTop = "40px";
+    a4_1.style.marginTop = "50px";
     cloud.style.transform = "translate(500px, 0px)";
     cloud.style.scale = "1.6";
     cloud.style.transition = "1s";
@@ -213,23 +296,37 @@ let a7_2 = document.querySelector(".article7-2");
 let a7_3 = document.querySelector(".article7-3");
 const a7 = [a7_1, a7_2, a7_3];
 
-let i1 = document.querySelector(".box1 .flex");
-let i2 = document.querySelector(".box2 .flex");
-let i3 = document.querySelector(".box3 .flex");
+let i1 = document.querySelector(".box1 .flex i");
+let hand = document.querySelector(".box1 .flex .hand");
+let i2 = document.querySelector(".box2 .flex i");
+let half = document.querySelector(".box2 .flex .half");
+let i3 = document.querySelector(".box3 .flex i");
+let energy = document.querySelector(".box3 .flex .energy");
 const i = [i1, i2, i3];
+const title = [hand, half, energy];
 const color = ["color_blue", "color_orange", "color_green"];
 
 for (let index = 0; index < 3; index++) {
-  if (!i[index].classList.contains(color[index])) {
+  if (
+    !i[index].classList.contains(color[index]) &&
+    !title[index].classList.contains(color[index])
+  ) {
     a7[index].addEventListener("mouseover", function () {
       i[index].classList.add(color[index]);
+      title[index].classList.add(color[index]);
       i[index].classList.remove("color_off");
+      title[index].classList.remove("color_off");
     });
   }
-  if (!i[index].classList.contains("color_off")) {
+  if (
+    !i[index].classList.contains("color_off") &&
+    !title[index].classList.contains("color_off")
+  ) {
     a7[index].addEventListener("mouseleave", function () {
       i[index].classList.remove(color[index]);
+      title[index].classList.remove(color[index]);
       i[index].classList.add("color_off");
+      title[index].classList.add("color_off");
     });
   }
 }
@@ -237,133 +334,77 @@ for (let index = 0; index < 3; index++) {
 // gray_header 이벤트
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
-const section1 = document.querySelector(".section1");
-const section3 = document.querySelector(".section3");
-const section4 = document.querySelector(".section4");
-const section5 = document.querySelector(".section5");
-const section6 = document.querySelector(".section6");
-const section7 = document.querySelector(".section7");
+let section1 = document.querySelector(".section1");
+let section3 = document.querySelector(".section3");
+let section4 = document.querySelector(".section4");
+let section5 = document.querySelector(".section5");
+let section6 = document.querySelector(".section6");
+let section7 = document.querySelector(".section7");
 
-const gray_header = document.querySelector(".gray_header");
-const backdrop = document.querySelector(".gray_header .backdrop");
-const menu1 = document.querySelector(".gray_header .menu1");
-const menu2 = document.querySelector(".gray_header .menu2");
-const menu3 = document.querySelector(".gray_header .menu3");
+let gray_header = document.querySelector(".gray_header");
+let backdrop = document.querySelector(".gray_header .backdrop");
+let menu1 = document.querySelector(".gray_header .menu1");
+let menu2 = document.querySelector(".gray_header .menu2");
+let menu3 = document.querySelector(".gray_header .menu3");
 
-const scrollHeight = body.scrollHeight;
+let scrollHeight = body.scrollHeight;
 let height1 = section1.offsetHeight;
 let height3 = section3.offsetHeight;
 let height4 = section4.offsetHeight;
 let height5 = section5.offsetHeight;
 let height6 = section6.offsetHeight;
 let height7 = section7.offsetHeight;
-const h1_3 = height1 + height3;
-const h1_4 = height1 + height3 + height4 + 200;
-const h1_5 = height1 + height3 + height4 + height5 + 350;
-const h1_7 = height1 + height3 + height4 + height5 + height6 + height7 + 300;
-
-// section3 스크롤하면 글씨 나타나는 애니메이션
-let a3_0 = document.querySelector(".article3-0");
-let a3_1 = document.querySelector(".article3-1");
-let a3_2 = document.querySelector(".article3-2");
-let a3_3 = document.querySelector(".article3-3");
-let a3_4 = document.querySelector(".article3-4");
-let a3_5 = document.querySelector(".article3-5");
-const a = [a3_0, a3_1, a3_2, a3_3, a3_4, a3_5];
-
-let a0 = a3_0.offsetHeight;
-let a1 = a3_1.offsetHeight;
-let a2 = a3_2.offsetHeight;
-let a3 = a3_3.offsetHeight;
-let a4 = a3_4.offsetHeight;
-let a5 = a3_5.offsetHeight;
-const a0_1 = a0 + a1;
-
-let margin = 700;
-let saTriggerHeight = 0;
-const saFunc = function () {
-  for (let index = 0; index < 6; index++) {
-    if (!a[index].classList.contains("show")) {
-      saTriggerHeight = a[index].getBoundingClientRect().top + margin;
-
-      if (window.innerHeight > saTriggerHeight) {
-        a[index].classList.add("show");
-      }
-    }
-  }
-};
-// window.addEventListener("onload", saFunc);
-// window.addEventListener("scroll", saFunc);
+let h1_3 = height1 + height3;
+let h1_4 = height1 + height3 + height4 + 200;
+let h1_5 = height1 + height3 + height4 + height5 + 350;
+let h1_7 = height1 + height3 + height4 + height5 + height6 + height7 + 300;
 
 window.addEventListener("scroll", () => {
-  console.log(scrollY);
-  if (scrollY > height1 && scrollY <= a0_1) {
-    a3_0.classList.add("sa");
-    a3_0.classList.add("show");
-  } else if (scrollY > a0 && scrollY <= a1) {
-    a3_1.classList.add("sa");
-    a3_1.classList.add("show");
-  } else if (scrollY > a1 && scrollY <= a2) {
-    a3_2.classList.add("sa");
-    a3_1.classList.add("show");
-  } else if (scrollY > a2 && scrollY <= a3) {
-    a3_3.classList.add("sa");
-    a3_1.classList.add("show");
-  } else if (scrollY > a3 && scrollY <= a4) {
-    a3_4.classList.add("sa");
-    a3_1.classList.add("show");
-  } else if (scrollY > a4 && scrollY <= a5) {
-    a3_5.classList.add("sa");
-    a3_1.classList.add("show");
+  if (scrollY <= h1_3) {
+    backdrop.style.backdropFilter = "blur(0px)";
+    gray_header.style.opacity = "0";
+    gray_header.style.transition = "0.5s";
+    section7.style.opacity = "1";
+    header.style.opacity = "1";
+  } else if (scrollY > h1_3 && scrollY <= h1_4) {
+    backdrop.style.backdropFilter = "blur(10px)";
+    gray_header.style.opacity = "1";
+    backdrop.style.backgroundColor = "rgba(220, 220, 220, 0.148)";
+    gray_header.style.transition = "0.3s";
+    menu1.style.transition = "0.1s";
+    menu1.classList.add("menu_on");
+    menu2.classList.remove("menu_on");
+    menu3.classList.remove("menu_on");
+    header.style.opacity = "0";
+    header.style.transition = "0.3s";
+    section7.style.opacity = "1";
+  } else if (scrollY > h1_4 && scrollY <= h1_5) {
+    backdrop.style.backdropFilter = "blur(10px)";
+    gray_header.style.opacity = "1";
+    backdrop.style.backgroundColor = "rgba(220, 220, 220, 0.148)";
+    gray_header.style.transition = "0.3s";
+    menu2.style.transition = "0.1s";
+    menu1.classList.remove("menu_on");
+    menu2.classList.add("menu_on");
+    menu3.classList.remove("menu_on");
+    header.style.opacity = "0";
+    section7.style.opacity = "1";
+  } else if (scrollY > h1_5 && scrollY <= h1_7) {
+    backdrop.style.backdropFilter = "blur(10px)";
+    gray_header.style.opacity = "1";
+    backdrop.style.backgroundColor = "rgba(220, 220, 220, 0.148)";
+    gray_header.style.transition = "0.3s";
+    menu3.style.transition = "0.1s";
+    menu1.classList.remove("menu_on");
+    menu2.classList.remove("menu_on");
+    menu3.classList.add("menu_on");
+    header.style.opacity = "0";
+    section7.style.opacity = "1";
+  } else if (scrollY > h1_7) {
+    gray_header.style.opacity = "0";
+    gray_header.style.transition = "0.5s";
+    section7.style.opacity = "0";
+    header.style.opacity = "0";
+    section7.style.transition = "0.5s";
   }
 });
-
-// window.addEventListener("scroll", () => {
-//   if (scrollY <= h1_3) {
-//     backdrop.style.backdropFilter = "blur(0px)";
-//     gray_header.style.opacity = "0";
-//     gray_header.style.transition = "0.5s";
-//     section7.style.opacity = "1";
-//     header.style.opacity = "1";
-//   } else if (scrollY > h1_3 && scrollY <= h1_4) {
-//     backdrop.style.backdropFilter = "blur(10px)";
-//     gray_header.style.opacity = "1";
-//     backdrop.style.backgroundColor = "rgba(220, 220, 220, 0.148)";
-//     gray_header.style.transition = "0.3s";
-//     menu1.style.transition = "0.1s";
-//     menu1.classList.add("menu_on");
-//     menu2.classList.remove("menu_on");
-//     menu3.classList.remove("menu_on");
-//     header.style.opacity = "0";
-//     header.style.transition = "0.3s";
-//     section7.style.opacity = "1";
-//   } else if (scrollY > h1_4 && scrollY <= h1_5) {
-//     backdrop.style.backdropFilter = "blur(10px)";
-//     gray_header.style.opacity = "1";
-//     backdrop.style.backgroundColor = "rgba(220, 220, 220, 0.148)";
-//     gray_header.style.transition = "0.3s";
-//     menu2.style.transition = "0.1s";
-//     menu1.classList.remove("menu_on");
-//     menu2.classList.add("menu_on");
-//     menu3.classList.remove("menu_on");
-//     header.style.opacity = "0";
-//     section7.style.opacity = "1";
-//   } else if (scrollY > h1_5 && scrollY <= h1_7) {
-//     backdrop.style.backdropFilter = "blur(10px)";
-//     gray_header.style.opacity = "1";
-//     backdrop.style.backgroundColor = "rgba(220, 220, 220, 0.148)";
-//     gray_header.style.transition = "0.3s";
-//     menu3.style.transition = "0.1s";
-//     menu1.classList.remove("menu_on");
-//     menu2.classList.remove("menu_on");
-//     menu3.classList.add("menu_on");
-//     header.style.opacity = "0";
-//     section7.style.opacity = "1";
-//   } else if (scrollY > h1_7) {
-//     gray_header.style.opacity = "0";
-//     gray_header.style.transition = "0.5s";
-//     section7.style.opacity = "0";
-//     header.style.opacity = "0";
-//     section7.style.transition = "0.5s";
-//   }
-// });
