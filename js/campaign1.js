ffcd1460bb498e4271a2760f4223d755a27b4142
gaeun9 <gaeun9809@naver.com>
2023-08-18 11:10:02 +0900

    배포 전 캠페인 업로드

---
+++ js/campaign1.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+  $(".up-button").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: $(".down").offset().top,
+      },
+      1000
+    );
+  });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $(".sub-box1-txt").each(function (i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: "1" }, 500);
+      }
+    });
+  });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $(".cam-grid9").each(function (i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: "1" }, 1000);
+      }
+    });
+  });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    $(".up-con4").each(function (i) {
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_object) {
+        $(this).animate({ opacity: "1" }, 1000);
+      }
+    });
+  });
+});
+
+$(function () {
+  $("button").click(function () {
+    $(".modal").fadeln();
+  });
+
+  $(".modal_content").click(function () {
+    $(".modal").fadeOut();
+  });
+});
