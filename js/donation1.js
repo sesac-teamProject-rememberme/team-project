930d5e6ea0f082b1e324f4a93d9a9163f04898a2
sennaseo <senna077@gmail.com>
2023-08-21 11:04:24 +0900

    fix

--- js/donation1.js
+++ js/donation1.js
@@ -46,39 +46,19 @@ window.addEventListener("scroll", () => {
   }
 });
 
-const slide = document.querySelector(".slide"); //전체 슬라이드 컨테이너
-const box = document.querySelectorAll(".slide .box"); //모든 슬라이드들
-let currentIdx = 0; //현재 슬라이드 index
-const slideCount = box.length; // 슬라이드 개수
-const prev = document.querySelector(".buttonprev"); //이전 버튼
-const next = document.querySelector(".buttonnext"); //다음 버튼
-const slideWidth = 300; //한개의 슬라이드 넓이
-const slideMargin = 100; //슬라이드간의 margin 값
-
-//전체 슬라이드 컨테이너 넓이 설정
-slides.style.width = (slideWidth + slideMargin) * slideCount + "300px";
-
-function moveSlide(num) {
-  slides.style.left = -num * 400 + "px";
-  currentIdx = num;
-}
-
-prev.addEventListener("click", function () {
-  /*첫 번째 슬라이드로 표시 됐을때는 
-  이전 버튼 눌러도 아무런 반응 없게 하기 위해 
-  currentIdx !==0일때만 moveSlide 함수 불러옴 */
-
-  if (currentIdx !== 0) moveSlide(currentIdx - 1);
-});
-
-next.addEventListener("click", function () {
-  /* 마지막 슬라이드로 표시 됐을때는 
-  다음 버튼 눌러도 아무런 반응 없게 하기 위해
-  currentIdx !==slideCount - 1 일때만 
-  moveSlide 함수 불러옴 */
-  if (currentIdx !== slideCount - 1) {
-    moveSlide(currentIdx + 1);
-  }
+const carousel = document.querySelector(".carousel");
+const box = document.querySelector(".box");
+const slide = document.querySelector(".slide");
+console.log(carousel);
+index = 1;
+index2 = 1;
+carousel.style.cursor = "pointer";
+let boxWidth = box.offsetWidth;
+console.log(boxWidth);
+carousel.addEventListener("click", function () {
+  slide.style.transform = `translate(${-boxWidth * index2}px,0%)`;
+  slide.style.transition = "0.6s cubic-bezier(0, 0.6, 0.4, 1)";
+  index2 += 1;
 });
 
 function image_onclick() {
